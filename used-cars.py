6462323abae04976924141f79df4a8b86da2be57
Abdullah Mushtaq <[Hello Worlds]>
2024-12-24 08:53:30 +0500

    added numpy

--- used-cars.py
+++ used-cars.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import numpy as np
 
 # Streamlit Page Configuration
 st.set_page_config(page_title="🚗 Used Car Market Analysis", layout="wide")
